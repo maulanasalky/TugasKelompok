b5164e99648db18db006c5498dd586f4b2b77477
maulanasalky <maulaansalky@students.amikom.ac.id>
2023-04-11 22:08:30 +0700

    first commit

---
+++ Dosen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasKelompok
+{
+    public class Dosen
+    {
+        private int Id, NIK;
+        private string Nama, Gender, Course;
+
+        public Dosen()
+        {
+            this.Id = 1;
+            this.Nama = "Dosen Pengampu";
+            this.NIK = 123456789;
+            this.Gender = "male";
+            this.Course = "Pemrograman Berprestasi Objek";
+        }
+
+        public void attDosen(string nama, int NIK)
+        {
+            this.Nama = Nama;
+            this.NIK = NIK;
+        }
+        
+        public void addCourse(string course)
+        {
+            this.Course += ", " + course;
+        }
+
+        public void Tampil()
+        {
+            Console.WriteLine($"ID :" +  Id);
+            Console.WriteLine($"Nama :" + Nama);
+            Console.WriteLine($"NIK :" + NIK);
+        }
+
+       
+           
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasKelompok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+
+            Console.WriteLine("Default Dosen Pengampu: ");
+            dosen.Tampil();
+
+            Console.WriteLine("\nMasukan Nama dan NIK Dosen Baru: ");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.Tampil();
+
+            Console.WriteLine("\nMasukan Mata Kuliah Baru: ");
+            dosen.addCourse(Console.ReadLine());
+            dosen.Tampil();
+
+            //Console.ReadLine;
+        }
+    }
+}
